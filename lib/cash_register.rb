df42b5303a528bb968f27d5e678518eedc7a4425
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-23 14:11:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
-
+class CashRegister 
+  
+  attr_accessor :discount, :total, :price, :items
+  
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end 
+  
+  def total 
+    @total 
+  end
+  
+  def add_item(item, price, quantity=1) 
+    @price = price 
+    @total += price * quantity
+    @items << item
+  end 
+  
+  def apply_discount(item,price)
+    
+  end 
+  
+  def items 
+    @items
+  end 
+  
+  def void_last_transaction
+    
+  end 
+  
+end 
