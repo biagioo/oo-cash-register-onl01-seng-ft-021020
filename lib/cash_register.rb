13d4e35df95549ce883fcd2c4502908fc8910d6f
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-23 15:03:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -39,10 +39,10 @@ class CashRegister
   
   
   def void_last_transaction
-    @total -= @price
     if @items = [] 
       @total -= @total
-      #binding.pry
+    else 
+      @total -= @price
     end
   end
   
