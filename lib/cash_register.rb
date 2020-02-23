10f21c4cfd77d83580c20f86a87ddf47cdddf48d
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-23 14:42:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,11 +12,19 @@ class CashRegister
     @total 
   end
   
-  def add_item(item, price, quantity=1) 
-    @price = price 
+  def add_item(item, price, quantity = 1)
+    @price = price
     @total += price * quantity
-    @items << item
-  end 
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter += 1
+      end
+    else
+      @items << item
+    end
+  end
   
   def apply_discount
     if @discount > 0
